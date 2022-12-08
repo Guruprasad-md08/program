bf90f1248faf948ee3333c27da7ded551dd96db9
Guruprasad_md08 <vasanthshetty775@gmail.com>
2022-12-08 19:26:41 +0530

    helloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+package program1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("hello world");
+	System.out.println("hello world");
+	System.out.println("hello world");
+	System.out.println("hello world");
+	System.out.println("program edited");
+	}
+
+}
